6373b7a0a7e1da402ba70c7ce0f4e7f7562269c6
jhjh550 <jhjh550@gmail.com>
2017-06-28 14:27:05 +0900

    alertcontroller addtextfield

--- SwiftTutorial20170627/AlertViewController.swift
+++ SwiftTutorial20170627/AlertViewController.swift
@@ -11,10 +11,25 @@ import UIKit
 class AlertViewController: UIViewController {
 
     @IBAction func onBtnClick(_ sender: Any) {
-        let alert = UIAlertController(title: "title", message: "message", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "title", message: "message", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "action title", style: .cancel, handler: nil)
         alert.addAction(action)
+        
+        alert.addTextField(configurationHandler: {(textField:UITextField) in
+            textField.placeholder = "input id"
+        })
+        
+        alert.addTextField(configurationHandler: {(textField:UITextField) in
+            textField.placeholder = "input password"
+            textField.isSecureTextEntry = true
+        })
+        
+//        let action2 = UIAlertAction(title: "action2", style: .default, handler: nil)
+//        alert.addAction(action2)
+//        
+//        let action3 = UIAlertAction(title: "action3", style: .destructive, handler: nil)
+//        alert.addAction(action3)
         
         present(alert, animated: true, completion: nil)
     }
