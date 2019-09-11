d7b9d5c4cb1ae41518cc2c637179d89b4fa74460
jh <jhjh550@gmail.com>
2019-09-11 15:29:22 +0900

    update alamofire

--- SwiftTutorial20170627/AlamofireViewController.swift
+++ SwiftTutorial20170627/AlamofireViewController.swift
@@ -21,21 +21,32 @@ class AlamofireViewController: UIViewController {
         super.viewDidLoad()
 
         
-        jsonParsing()
+        jsonParsing2()
         
         
 
-        Alamofire.request("https://httpbin.org/get").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-            }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
+//        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+//
+//            if let json = response.result.value {
+//                print("JSON: \(json)") // serialized json response
+//            }
+//
+//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+//                print("Data: \(utf8Text)") // original server data as UTF8 string
+//            }
+//        }
+    }
+    
+    func jsonParsing2(){
+        Alamofire.request("https://restcountries.eu/rest/v2/all").responseJSON { (response) in
+            do{
+                let coutries = try JSONDecoder().decode([Country].self, from: response.data!)
+                print(coutries)
+            }catch{
+                print(error.localizedDescription)
             }
         }
     }
